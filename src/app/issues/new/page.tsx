5bbed18145c09313d9a91d2435145c5cc3af757d
conrad mugabe <conradmugabe@gmail.com>
2023-10-10 00:36:13 +0300

    feat: added functionality to issue form

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -2,16 +2,33 @@
 
 import SimpleMDE from "react-simplemde-editor";
 import { Button, TextField } from "@radix-ui/themes";
+import { Controller, useForm } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
 
+type IssueForm = {
+  title: string;
+  description: string;
+};
+
 export default function NewIssuePage() {
+  const { control, handleSubmit, register } = useForm<IssueForm>();
+
   return (
-    <div className="max-w-xl space-y-3">
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit((data) => console.log(data))}
+    >
       <TextField.Root>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input placeholder="Title" {...register("title")} />
       </TextField.Root>
-      <SimpleMDE placeholder="Description" />
+      <Controller
+        control={control}
+        name="description"
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 }
