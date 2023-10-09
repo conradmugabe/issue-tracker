7c35e63dcfcf72876b80bd375c290c91575f522f
conrad mugabe <conradmugabe@gmail.com>
2023-10-10 00:31:20 +0300

    feat: added markdown-editor for issue description

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import { Button, TextField } from "@radix-ui/themes";
+import "easymde/dist/easymde.min.css";
 
 export default function NewIssuePage() {
   return (
@@ -8,7 +10,7 @@ export default function NewIssuePage() {
       <TextField.Root>
         <TextField.Input placeholder="Title" />
       </TextField.Root>
-      <TextArea placeholder="Description" />
+      <SimpleMDE placeholder="Description" />
       <Button>Submit New Issue</Button>
     </div>
   );
