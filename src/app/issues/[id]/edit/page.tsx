3076c4d6419cb1a150d37ec7be97d83443016011
conrad mugabe <conradmugabe@gmail.com>
2023-10-10 14:12:38 +0300

    feat: added edit issue form

--- src/app/issues/[id]/edit/page.tsx
+++ src/app/issues/[id]/edit/page.tsx
@@ -1,7 +1,18 @@
+import { notFound } from "next/navigation";
+
+import prisma from "@/db/prisma";
+import { IssueForm } from "@/app/issues/_components/issue-form";
+
 type Props = {
   params: { id: string };
 };
 
-export default function EditIssuePage({ params }: Props) {
-  return <h1>{params.id}</h1>;
+export default async function EditIssuePage({ params }: Props) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: Number(params.id) },
+  });
+
+  if (!issue) notFound();
+
+  return <IssueForm issue={issue} />;
 }
---
+++ src/app/issues/_components/issue-form.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import axios from "axios";
+import SimpleMDE from "react-simplemde-editor";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import "easymde/dist/easymde.min.css";
+
+import { createIssueSchema } from "@/issues/entities/dto";
+import { ErrorMessage, Spinner } from "@/components";
+import { Issue } from "@prisma/client";
+
+type IssueFormData = z.infer<typeof createIssueSchema>;
+
+export function IssueForm({ issue }: { issue?: Issue }) {
+  const router = useRouter();
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm<IssueFormData>({
+    defaultValues: { title: issue?.title, description: issue?.description },
+    resolver: zodResolver(createIssueSchema),
+  });
+  const [error, setError] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setError("");
+      setIsSubmitting(true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setIsSubmitting(false);
+      setError("An unexpected error occurred. Try again later.");
+    }
+  });
+
+  return (
+    <div className="max-w-xl">
+      {error ? (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      ) : null}
+      <form className="space-y-3" onSubmit={onSubmit}>
+        <TextField.Root>
+          <TextField.Input placeholder="Title" {...register("title")} />
+        </TextField.Root>
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+        <Controller
+          control={control}
+          name="description"
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+        <Button className="cursor-pointer" disabled={isSubmitting}>
+          {issue ? "Update Issue" : "Submit New Issue"}{" "}
+          {isSubmitting ? <Spinner /> : null}
+        </Button>
+      </form>
+    </div>
+  );
+}
