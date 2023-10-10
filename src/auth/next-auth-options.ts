433e7441119790d7a28f730794243aa0b7b44689
conrad mugabe <conradmugabe@gmail.com>
2023-10-10 22:13:18 +0300

    feat: added prisma-next-auth-adapter

--- src/auth/next-auth-options.ts
+++ src/auth/next-auth-options.ts
@@ -1,11 +1,16 @@
-import { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { AuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+import prisma from "@/db/prisma";
 
 export const nextAuthOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  session: { strategy: "jwt" },
 };
