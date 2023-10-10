21d4e6c653a027b6c957f02fd42832425611536d
conrad mugabe <conradmugabe@gmail.com>
2023-10-10 15:02:35 +0300

    refactor: moved all loading components to use custom skeleton

--- src/app/issues/loading.tsx
+++ src/app/issues/loading.tsx
@@ -1,6 +1,6 @@
-import Skeleton from "react-loading-skeleton";
 import { Table } from "@radix-ui/themes";
-import "react-loading-skeleton/dist/skeleton.css";
+
+import { Skeleton } from "@/components";
 
 export default function LoadingIssuesPage() {
   const issues = [1, 2, 3, 4, 5];
