e8b76bc401049c5ec84f6222ed35338a76001950
conrad mugabe <conradmugabe@gmail.com>
2024-05-27 20:25:49 +0300

    feat: switched db providers

--- prisma/migrations/20231012122305_added_assignee_user_field_to_issue_model/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Issue" ADD COLUMN     "assigneeUserId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "Issue" ADD CONSTRAINT "Issue_assigneeUserId_fkey" FOREIGN KEY ("assigneeUserId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
